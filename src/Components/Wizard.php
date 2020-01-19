37b8356e92b4647f3e506012bdaa1d14a50226f8
jinyicheng <jinyicheng@live.com>
2020-01-19 16:47:55 +0800

    修复部分引用错误

--- src/Components/Wizard.php
+++ src/Components/Wizard.php
@@ -1,152 +1,207 @@
 <?php
 
 namespace SmartUI\Components;
-use \SmartUI\UI;
-use \SmartUI\Util;
 
-class Wizard extends UI {
+use ReflectionException;
+use SmartUI\UI;
+use SmartUI\Util;
 
-	const WIZARD_BOOTSTRAP = 'bootstrap';
-	const WIZARD_FUEL = 'fuel';
+class Wizard extends UI
+{
 
-	private $_options_map = array(
-		'in_widget' => true,
-		'type' => Wizard::WIZARD_BOOTSTRAP
-	);
+    const WIZARD_BOOTSTRAP = 'bootstrap';
+    const WIZARD_FUEL = 'fuel';
 
-	private $_structure = array(
-		'options' => [],
-		'step' => [],
-		'content' => [],
-		'widget' => null,
-		'active' => [],
-		'title' => '',
-		'tabl_class' => 'form-wizard',
-		'id' => ''
-	);
+    private $_options_map = array(
+        'in_widget' => true,
+        'type' => Wizard::WIZARD_BOOTSTRAP
+    );
 
-	public function __construct($steps, $options = []) {
-		$this->_init_structure($steps, $options);
-	}
+    private $_structure = array(
+        'options' => [],
+        'step' => [],
+        'content' => [],
+        'widget' => null,
+        'active' => [],
+        'title' => '',
+        'tabl_class' => 'form-wizard',
+        'id' => ''
+    );
 
-	private function _init_structure($steps, $user_options = []) {
-		$this->_structure = Util::array_to_object($this->_structure);
-		$this->_structure->options = Util::set_array_prop_def($this->_options_map, $user_options);
-		$this->_structure->step = $steps;
-		$this->_structure->id = Util::create_id(true);
-		$widget = new Widget();
-		$widget->options('editbutton', false)
-		    ->header('title', '<h2></h2>');
+    /**
+     * Wizard constructor.
+     * @param $steps
+     * @param array $options
+     */
+    public function __construct($steps, $options = [])
+    {
+        $this->_init_structure($steps, $options);
+    }
 
-		$this->_structure->widget = $widget;
-	}
+    /**
+     * @param $steps
+     * @param array $user_options
+     */
+    private function _init_structure($steps, $user_options = [])
+    {
+        $this->_structure = Util::array_to_object($this->_structure);
+        $this->_structure->options = Util::set_array_prop_def($this->_options_map, $user_options);
+        $this->_structure->step = $steps;
+        $this->_structure->id = Util::create_id(true);
+        $widget = new Widget();
+        $widget->options('editbutton', false)
+            ->header('title', '<h2></h2>');
 
-	public function __get($name) {
-		if (isset($this->_structure->{$name})) {
+        $this->_structure->widget = $widget;
+    }
+
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        if (isset($this->_structure->{$name})) {
             return $this->_structure->{$name};
         }
-        parent::err('Undefined structure property: '.$name);
+        parent::err('Undefined structure property: ' . $name);
         return null;
-	}
+    }
 
-	public function __set($name, $value) {
-		if (isset($this->_structure->{$name})) {
+    /**
+     * @param $name
+     * @param $value
+     */
+    public function __set($name, $value)
+    {
+        if (isset($this->_structure->{$name})) {
             $this->_structure->{$name} = $value;
             return;
         }
-		parent::err('Undefined structure property: '.$name);
-	}
+        parent::err('Undefined structure property: ' . $name);
+    }
 
-	public function __call($name, $args) {
-		return parent::_call($this, $this->_structure, $name, $args);
-	}
+    /**
+     * @param $name
+     * @param $args
+     * @return mixed|object|void|null
+     * @throws ReflectionException
+     */
+    public function __call($name, $args)
+    {
+        return parent::_call($this, $this->_structure, $name, $args);
+    }
 
-	private function _get_bootstrap_result() {
-		
+    /**
+     * @param bool $return
+     * @return string|null|void
+     */
+    public function print_html($return = false)
+    {
+        switch ($this->_structure->options["type"]) {
+            case Wizard::WIZARD_BOOTSTRAP:
+                $result = self::_get_bootstrap_result();
+                break;
 
-		$structure = $this->_structure;
+            default:
+                parent::err('SmartUI::Wizard Invalid Wizard Type: wizard "' . $this->_structure->options["type"] . '" not found.');
+                break;
+        }
 
-		$steps = Util::get_property_value($structure->step, array(
-			'if_closure' => function($steps) {
-				return Util::run_callback($steps, array($this));
-			},
-			'if_other' => function($steps) {
-				parent::err('SmartUI::Wizard::step requires array');
-				return null;
-			}
-		));
+        if ($return) return $result;
+        else echo $result;
+    }
 
-		if (!is_array($steps)) {
-			parent::err("SmartUI::Wizard::step requires array");
-			return null;
-		}
+    /**
+     * @return string|null
+     */
+    private function _get_bootstrap_result()
+    {
 
-		$li_list = [];
-		$step_content_list = [];
-		$has_active = false;
-		$step = 1;
-		foreach ($steps as $step_id => $step_prop) {
-			$step_structure = array(
-				'content' => isset($structure->content[$step_id]) ? $structure->content[$step_id] : '',
-				'title' => isset($structure->title[$step_id]) ? $structure->title[$step_id] : '',
-				'step' => $step,
-				'active' => isset($structure->active[$step_id]) && $structure->active[$step_id] === true,
-			);
 
-			$new_step_prop = Util::get_clean_structure($step_structure, $step_prop, array($this, $steps, $step_id), 'title');
+        $structure = $this->_structure;
 
-			foreach ($new_step_prop as $step_prop_key => $step_prop_vaue) {
-				$new_step_prop_value = Util::get_property_value($step_prop_vaue, array(
-					'if_closure' => function($prop_value) use ($steps) {
-						return Util::run_callback($prop_value, array($this, $steps));
-					}
-				));
-				$new_step_prop[$step_prop_key] = $new_step_prop_value;
-			}
+        $steps = Util::get_property_value($structure->step, array(
+            'if_closure' => function ($steps) {
+                return Util::run_callback($steps, array($this));
+            },
+            'if_other' => function ($steps) {
+                parent::err('SmartUI::Wizard::step requires array');
+                return null;
+            }
+        ));
 
-			$step_content_classes = [];
-			$step_content_classes[] = 'tab-pane';
-			$li_classes = [];
-			$a_classes = [];
-			$a_attr = [];
+        if (!is_array($steps)) {
+            parent::err("SmartUI::Wizard::step requires array");
+            return null;
+        }
 
-			if ((!$structure->active && !$has_active) || ($new_step_prop['active'] === true && !$has_active)) {
-				$li_classes[] = 'active';
-				$step_content_classes[] = 'active';
-				$has_active = true;
-			}
+        $li_list = [];
+        $step_content_list = [];
+        $has_active = false;
+        $step = 1;
+        foreach ($steps as $step_id => $step_prop) {
+            $step_structure = array(
+                'content' => isset($structure->content[$step_id]) ? $structure->content[$step_id] : '',
+                'title' => isset($structure->title[$step_id]) ? $structure->title[$step_id] : '',
+                'step' => $step,
+                'active' => isset($structure->active[$step_id]) && $structure->active[$step_id] === true,
+            );
 
-			$title = $new_step_prop['title'];
+            $new_step_prop = Util::get_clean_structure($step_structure, $step_prop, array($this, $steps, $step_id), 'title');
 
-			$href = '#'.$step_id;
-			$a_attr[] = 'data-toggle="tab"';
+            foreach ($new_step_prop as $step_prop_key => $step_prop_vaue) {
+                $new_step_prop_value = Util::get_property_value($step_prop_vaue, array(
+                    'if_closure' => function ($prop_value) use ($steps) {
+                        return Util::run_callback($prop_value, array($this, $steps));
+                    }
+                ));
+                $new_step_prop[$step_prop_key] = $new_step_prop_value;
+            }
 
-			$class = $li_classes ? ' class="'.implode(' ', $li_classes).'"' : '';
+            $step_content_classes = [];
+            $step_content_classes[] = 'tab-pane';
+            $li_classes = [];
+            $a_classes = [];
+            $a_attr = [];
 
-			$li_html = '<li'.$class.' data-target="#'.$step_id.'">';
-			$li_html .= '<a href="'.$href.'" '.($a_classes ? 'class="'.implode(' ', $a_classes).'"' : '').($a_attr ? ' '.implode(' ', $a_attr) : '').'>';
-			$li_html .= '<span class="step">'.$new_step_prop['step'].'</span> <span class="title">'.$title.'</span>';
-			$li_html .= '</a>';
-			$li_html .= '</li>';
-			$li_list[] = $li_html;
+            if ((!$structure->active && !$has_active) || ($new_step_prop['active'] === true && !$has_active)) {
+                $li_classes[] = 'active';
+                $step_content_classes[] = 'active';
+                $has_active = true;
+            }
 
-			$step_content_html = '<div class="'.implode(' ', $step_content_classes).'" id="'.$step_id.'">';
-			$step_content_html .= $new_step_prop['content'];
-			$step_content_html .= '</div>';
-			$step_content_list[] = $step_content_html;
+            $title = $new_step_prop['title'];
 
-			$step++;
-		}
+            $href = '#' . $step_id;
+            $a_attr[] = 'data-toggle="tab"';
 
-		$ul_classes = [];
-		$ul_classes[] = 'bootstrapWizard';
-		$ul_classes[] = $structure->tabl_class;
+            $class = $li_classes ? ' class="' . implode(' ', $li_classes) . '"' : '';
 
-		$content_classes = [];
-		$content_classes[] = 'tab-content';
-		$content_html = '<div class="'.implode(' ', $content_classes).'">';
-		$content_html .= 	implode('', $step_content_list);
-		$content_html .= '		<div class="form-actions">
+            $li_html = '<li' . $class . ' data-target="#' . $step_id . '">';
+            $li_html .= '<a href="' . $href . '" ' . ($a_classes ? 'class="' . implode(' ', $a_classes) . '"' : '') . ($a_attr ? ' ' . implode(' ', $a_attr) : '') . '>';
+            $li_html .= '<span class="step">' . $new_step_prop['step'] . '</span> <span class="title">' . $title . '</span>';
+            $li_html .= '</a>';
+            $li_html .= '</li>';
+            $li_list[] = $li_html;
+
+            $step_content_html = '<div class="' . implode(' ', $step_content_classes) . '" id="' . $step_id . '">';
+            $step_content_html .= $new_step_prop['content'];
+            $step_content_html .= '</div>';
+            $step_content_list[] = $step_content_html;
+
+            $step++;
+        }
+
+        $ul_classes = [];
+        $ul_classes[] = 'bootstrapWizard';
+        $ul_classes[] = $structure->tabl_class;
+
+        $content_classes = [];
+        $content_classes[] = 'tab-content';
+        $content_html = '<div class="' . implode(' ', $content_classes) . '">';
+        $content_html .= implode('', $step_content_list);
+        $content_html .= '		<div class="form-actions">
 									<div class="row">
 										<div class="col-sm-12">
 											<ul class="pager wizard no-margin">
@@ -166,52 +221,37 @@ class Wizard extends UI {
 										</div>
 									</div>
 								</div>';
-		$content_html .= '</div>';
+        $content_html .= '</div>';
 
 
+        $ul_html = '<div class="form-bootstrapWizard">';
+        $ul_html .= '	<ul class="' . implode(' ', $ul_classes) . '">';
+        $ul_html .= implode('', $li_list);
+        $ul_html .= '	</ul>';
+        $ul_html .= '	<div class="clearfix"></div>';
+        $ul_html .= '</div>';
 
-		$ul_html = '<div class="form-bootstrapWizard">';
-		$ul_html .= '	<ul class="'.implode(' ', $ul_classes).'">';
-		$ul_html .= 		implode('', $li_list);
-		$ul_html .= '	</ul>';
-		$ul_html .= '	<div class="clearfix"></div>';
-		$ul_html .= '</div>';
-
-		$main_content_html = '<div class="row">';
-		$main_content_html .= '		<div class="col-sm-12" id="'.$structure->id.'">';
-		$main_content_html .= 			$ul_html.$content_html;
-		$main_content_html .= '		</div>';
-		$main_content_html .= '</div>';
+        $main_content_html = '<div class="row">';
+        $main_content_html .= '		<div class="col-sm-12" id="' . $structure->id . '">';
+        $main_content_html .= $ul_html . $content_html;
+        $main_content_html .= '		</div>';
+        $main_content_html .= '</div>';
 
 
-		if (isset($structure->options["in_widget"]) && $structure->options["in_widget"]) {
-			$structure->widget->body("content", $main_content_html);
-			if ($structure->title) {
-				$structure->widget->header('title', $structure->title);
-			}
+        if (isset($structure->options["in_widget"]) && $structure->options["in_widget"]) {
+            $structure->widget->body("content", $main_content_html);
+            if ($structure->title) {
+                $structure->widget->header('title', $structure->title);
+            }
 
-			$result = $structure->widget->print_html(true);
-		} else $result = $main_content_html;
+            $result = $structure->widget->print_html(true);
+        } else $result = $main_content_html;
 
-		return $result;
-	}
+        return $result;
+    }
 
-	private function _get_fuel_result() {
+    private function _get_fuel_result()
+    {
 
-	}
-
-	public function print_html($return = false) {
-		switch ($this->_structure->options["type"]) {
-			case Wizard::WIZARD_BOOTSTRAP:
-				$result = self::_get_bootstrap_result();
-				break;
-
-			default:
-				parent::err('SmartUI::Wizard Invalid Wizard Type: wizard "'.$this->_structure->options["type"].'" not found.');
-				break;
-		}
-
-		if ($return) return $result;
-		else echo $result;
-	}
+    }
 }
--- src/Util.php
+++ src/Util.php
@@ -6,9 +6,14 @@ use Closure;
 use ReflectionException;
 use ReflectionFunction;
 
-class Util {
-
-    public static function is_assoc($array) {
+class Util
+{
+    /**
+     * @param $array
+     * @return bool
+     */
+    public static function is_assoc($array)
+    {
         foreach (array_keys($array) as $k => $v) {
             if ($k !== $v)
                 return true;
@@ -16,46 +21,61 @@ class Util {
         return false;
     }
 
-	public static function clean_html_string($str_value, $nl2br = true) {
+    /**
+     * @param $str_value
+     * @param bool $nl2br
+     * @return string
+     */
+    public static function clean_html_string($str_value, $nl2br = true)
+    {
         if (is_null($str_value)) $str_value = "";
         $new_str = is_string($str_value) ? htmlentities(html_entity_decode($str_value, ENT_QUOTES)) : $str_value;
         $new_str = utf8_encode($new_str);
         return $nl2br ? nl2br($new_str) : $new_str;
     }
 
-    public static function is_closure($obj) {
-        return (is_object($obj) && ($obj instanceof Closure));
-    }
-
-    public static function array_to_object($array, $recursive = false) {
-	    if (!is_object($array) && !is_array($array))
-	        return $array;
-
-        if (!$recursive) return (object)$array;
-
-	    if (is_array($array))
-	    	return (object)array_map(array(__CLASS__, 'array_to_object'), $array);
-	    else return $array;
-	}
-
-    public static function object_to_array($object) {
-        if (!is_object($object) && !is_array($object))
-            return $object;
-
-        if (is_object($object))
-            $object = get_object_vars($object);
-
-        return array_map(array(__CLASS__, 'object_to_array'), $object);
-    }
-
-    public static function create_id($md5 = false) {
+    /**
+     * @param bool $md5
+     * @return string|string[]
+     */
+    public static function create_id($md5 = false)
+    {
         $uid = uniqid((double)microtime() * 10000, true);
         $result = str_replace(".", "", $uid);
         $result = $md5 ? md5($result) : $result;
         return $result;
     }
 
-    public static function get_property_value($prop, $prop_methods) {
+    /**
+     * @param $default_prop
+     * @param $value
+     * @param array $closure_defaults
+     * @param string $default_key
+     * @return array
+     */
+    public static function get_clean_structure($default_prop, $value, $closure_defaults = [], $default_key = '')
+    {
+        return self::get_property_value($value, array(
+            'if_array' => function ($value) use ($default_prop, $default_key) {
+                return self::set_array_prop_def($default_prop, $value, $default_key);
+            },
+            'if_closure' => function ($value) use ($closure_defaults, $default_prop, $default_key) {
+                return self::set_closure_prop_def($default_prop, $value, $closure_defaults, $default_key);
+            },
+            'if_other' => function ($value) use ($default_prop, $default_key) {
+                $default_prop[$default_key] = $value;
+                return $default_prop;
+            }
+        ));
+    }
+
+    /**
+     * @param $prop
+     * @param $prop_methods
+     * @return array
+     */
+    public static function get_property_value($prop, $prop_methods)
+    {
         if (self::is_closure($prop)) {
             return isset($prop_methods['if_closure']) ? $prop_methods['if_closure']($prop) : $prop($prop);
         } else if (is_array($prop) || is_object($prop)) {
@@ -67,22 +87,62 @@ class Util {
         }
     }
 
-    public static function get_clean_structure($default_prop, $value, $closure_defaults = [], $default_key = '') {
-        return self::get_property_value($value, array(
-            'if_array' => function ($value) use ($default_prop, $default_key) {
-                return self::set_array_prop_def($default_prop, $value, $default_key);
-            },
-            'if_closure' => function($value) use ($closure_defaults, $default_prop, $default_key) {
-                return self::set_closure_prop_def($default_prop, $value, $closure_defaults, $default_key);
-            },
-            'if_other' => function($value) use ($default_prop, $default_key) {
-                $default_prop[$default_key] = $value;
-                return $default_prop;
-            }
-        ));
+    /**
+     * @param $obj
+     * @return bool
+     */
+    public static function is_closure($obj)
+    {
+        return (is_object($obj) && ($obj instanceof Closure));
     }
 
-    public static function set_closure_prop_def($default_structure, $callback_value, $callback_defaults = [], $set_to_key_if_fail = "") {
+    /**
+     * @param $object
+     * @return array
+     */
+    public static function object_to_array($object)
+    {
+        if (!is_object($object) && !is_array($object))
+            return $object;
+
+        if (is_object($object))
+            $object = get_object_vars((object)$object);
+
+        return array_map(array(__CLASS__, 'object_to_array'), $object);
+    }
+
+    /**
+     * @param $default_structure
+     * @param $array_value
+     * @param string $set_to_key_if_fail
+     * @return mixed
+     */
+    public static function set_array_prop_def($default_structure, $array_value, $set_to_key_if_fail = "")
+    {
+        if ($set_to_key_if_fail != "") {
+            if (!is_array($array_value)) {
+                if (isset($default_structure[$set_to_key_if_fail]))
+                    $default_structure[$set_to_key_if_fail] = $array_value;
+                return $default_structure;
+            }
+        }
+
+        foreach ($array_value as $key => $value) {
+            $default_structure[$key] = $value;
+        }
+        return $default_structure;
+    }
+
+    /**
+     * @param $default_structure
+     * @param $callback_value
+     * @param array $callback_defaults
+     * @param string $set_to_key_if_fail
+     * @return mixed
+     * @throws ReflectionException
+     */
+    public static function set_closure_prop_def($default_structure, $callback_value, $callback_defaults = [], $set_to_key_if_fail = "")
+    {
         if ($set_to_key_if_fail != "") {
             if (!self::is_closure($callback_value)) {
                 if (isset($default_structure[$set_to_key_if_fail]))
@@ -101,28 +161,14 @@ class Util {
         return $default_structure;
     }
 
-    public static function set_array_prop_def($default_structure, $array_value, $set_to_key_if_fail = "") {
-        if ($set_to_key_if_fail != "") {
-            if (!is_array($array_value)) {
-                if (isset($default_structure[$set_to_key_if_fail]))
-                    $default_structure[$set_to_key_if_fail] = $array_value;
-                return $default_structure;
-            }
-        }
-
-        foreach ($array_value as $key => $value) {
-            $default_structure[$key] = $value;
-        }
-        return $default_structure;
-    }
-
     /**
      * @param $callback
      * @param $default_args
      * @return mixed
      * @throws ReflectionException
      */
-    public static function run_callback($callback, $default_args) {
+    public static function run_callback($callback, $default_args)
+    {
         $reflection = new ReflectionFunction($callback);
         $params = $reflection->getParameters();
         if (!$params || !$default_args) return call_user_func($callback);
@@ -138,17 +184,35 @@ class Util {
         return call_user_func_array($callback, $ref_args);
     }
 
-    public static function replace_col_codes($str, $row, $url_encode=false) {
+    public static function replace_col_codes($str, $row, $url_encode = false)
+    {
         preg_match_all("/\{([^&={{}}]+)\}/", $str, $matched_cols);
         $col_replace = [];
         $col_search = [];
-        foreach($matched_cols[1] as $matched_col) {
+        foreach ($matched_cols[1] as $matched_col) {
             if (is_array($row)) $row = self::array_to_object($row);
             if (isset($row->{$matched_col})) {
                 $col_replace[] = $url_encode ? urlencode($row->{$matched_col}) : $row->{$matched_col};
-                $col_search[] = "/{{".$matched_col."}}/";
+                $col_search[] = "/{{" . $matched_col . "}}/";
             }
         }
         return preg_replace($col_search, $col_replace, $str);
+    }
+
+    /**
+     * @param $array
+     * @param bool $recursive
+     * @return array|object
+     */
+    public static function array_to_object($array, $recursive = false)
+    {
+        if (!is_object($array) && !is_array($array))
+            return $array;
+
+        if (!$recursive) return (object)$array;
+
+        if (is_array($array))
+            return (object)array_map(array(__CLASS__, 'array_to_object'), $array);
+        else return $array;
     }
 }
